0e6528d9dfd7a22dd38e436a8d3b76326d18a42b
eddy <eddy.lxj@gmail.com>
2019-05-18 16:49:04 -0400

    init project

---
+++ running_location/src/main/java/demo/domain/Location.java
@@ -0,0 +1,51 @@
+package demo.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "RUNNING_LOCATIONS")
+public class Location {
+    enum GpsStatus{
+        EXCELLENT, OK, UNRELIABLE, BAD, NOFIX, UNKNOWN;
+    }
+
+    public enum RunnerMovementType{
+        STOPPED, IN_MOTION;
+
+        public boolean isMoving(){
+            return this != STOPPED;
+        }
+    }
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Embedded
+    @AttributeOverride(name="bandMake", column = @Column(name = "unit_band_make"))
+    private final UnitInfo unitInfo;
+
+    private MedicalInfo medicalInfo;
+
+    private double latitude;
+    private double longtitude;
+
+    private String heading;
+    private double gpsSpeed;
+    private GpsStatus gpsStatus;
+
+    private double odometer;
+    private double totalRunningTime;
+    private double totalIdleTime;
+    private double totalCaloriesBurnt;
+    private String address;
+    private Date timestamp = new Date();
+    private String gearProvider;
+    private RunnerMovementType runnerMovementType = RunnerMovementType.STOPPED;
+    private String serviceType;
+
+    public Location(String runningId){
+        this.unitInfo = new UnitInfo(runningId);
+    }
+}
